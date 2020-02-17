ccbbd255ba57fb8d2a4b461313251c3bd4565732
Arthur Bianchini <ArtBianchini@gmail.com>
2020-02-17 18:33:44 +0000

    L21 on Mello

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,11 +1,21 @@
+const $submitButton = $('#submit');
 const $setLogin = $('#login');
 const $setSignUp = $('#signup');
-const $submitButton = $('#submit');
 const $emailInput = $('#email');
 const $passwordInput = $('#password');
 const $message = $('#message');
 
 let authSetting = 'login';
+
+
+sendUserToBoards();
+
+function sendUserToBoards() {
+  if (localStorage.getItem('user')) {
+    location.replace('/boards');
+  }
+}
+
 
 function setAuth(setting) {
   authSetting = setting;
@@ -22,8 +32,7 @@ function setAuth(setting) {
 }
 
 function handleFormSubmit(event) {
-  event.preventDefault(); 
-
+  event.preventDefault();
 
   let email = $emailInput.val().trim();
   let password = $passwordInput.val().trim();
@@ -33,19 +42,21 @@ function handleFormSubmit(event) {
     return;
   }
 
+  $emailInput.val('');
+  $passwordInput.val('');
 
-    console.log(
-    `Email: ${email} Password: ${password} AuthSetting: ${authSetting}`
-  );
+  authenticateUser(email, password);
 }
 
 function displayMessage(message, type) {
   $message.text(message).attr('class', type);
 }
 
+
 function handleSignupResponse(status) {
   if (status === 'success') {
     displayMessage('Registered successfully! You may now sign in.', 'success');
+    setAuth('login'); 
   } else {
     displayMessage(
       'Something went wrong. A user with this account may already exist.',
@@ -54,6 +65,18 @@ function handleSignupResponse(status) {
   }
 }
 
+function handleLoginResponse(data, status, jqXHR) {
+  if (status === 'success') {
+    let jwt = jqXHR.getResponseHeader('authorization');
+    let user = JSON.stringify(data);
+
+    localStorage.setItem('authorization', jwt);
+    localStorage.setItem('user', user);
+    sendUserToBoards(); 
+  } else {
+    displayMessage('Invalid email or password.', 'danger');
+  }
+}
 
 function authenticateUser(email, password) {
   $.ajax({
@@ -66,14 +89,18 @@ function authenticateUser(email, password) {
     },
     method: 'POST'
   })
-  .then(function(data, status) {
+  .then(function(data, status, jqXHR) {
       if (authSetting === 'signup') {
         handleSignupResponse(status);
+      } else {
+        handleLoginResponse(data, status, jqXHR);
       }
     })
     .catch(function(err) {
       if (authSetting === 'signup') {
         handleSignupResponse(err.statusText);
+      } else {
+        handleLoginResponse(err.statusText);
       }
     });
 }
